c612a1448476a871e45f5bac5bd57d90c41402fd
jakal1989 <stefano.cugis@gmail.com>
2020-10-25 11:42:37 +0100

    Update motor.py

--- motor.py
+++ motor.py
@@ -1,35 +1,48 @@
+# Importieren von verschidenen Bibliotheken
 import RPi.GPIO as GPIO
 import time
 
+# Benutzung von "Broadcom SOC channel" PIN-Schema
 GPIO.setmode(GPIO.BCM)
-#Define PIN 17 as OUT GPIO (Servomotor)
+
+# Definiere PIN 17 als Ausgang GPIO (Motor)
 GPIO.setup(17, GPIO.OUT)
-#Define Name and PWM (50Hz)for PIN 17,
+# Instanziere GPIO 17 mit dem Namen servo und bestimme Frequenz 50Hz
 servo = GPIO.PWM(17,50)
 
+# Klasse Namens Motor definieren
 class Motor:
+    # Statische Methode Namens run() definieren
     @staticmethod
     def run():
-        # Start Motor
-        # Cycle 2.5 = 0 degrees / 5 = 45 degrees / 7.5 = 90 degrees / 10 = 135 degrees / 12.5 = 180 degrees
-        # start PWM running, check if Servo is set to 180
         print ("Set Servo to 180 degrees (Slider out of tray)")
-        servo.start(12.5) # Initialisierung
+        # Initialisierung setzte den Motor auf 180 Grad
+        servo.start(12.5)
+        # Warte 2.5 Sekunden
         time.sleep(2.5)
-        print ("Set Servo to 109 degrees (Slider in tray)")
+        print ("Set Servo to 99 degrees (Slider in tray)")
+        # Setzte den Motor auf 99 Grad
         servo.ChangeDutyCycle(8.0)
+        # Warte 2.5 Sekunden
         time.sleep(2.5)
         print ("Turning back to 180 degrees (Slider out of tray)")
+        # Setzte den Motor auf 180 Grad
         servo.ChangeDutyCycle(12.5)
+         # Warte 4 Sekunden
         time.sleep(4.0)
+    # Statische Methode Namens positions_check() definieren
     @staticmethod
     def position_check():
-        # Set motor to 180 degees if in last run, an Error appeard an motor is no out of tray
         print ("Set Servo to 180 degrees (Check if Servo is out of tray)")
-        servo.start(12.5) # Initialisierung
+        # Initialisierung setzte den Motor auf 180 Grad
+        servo.start(12.5)
+        # Warte 2.5 Sekunden
         time.sleep(2.5)
+    # Statische Methode Namens stop() definieren
     @staticmethod   
     def stop():
+        # Motor anhalten und auf Null stellen
         servo.stop()
         print ("Stop Motor")
+        # Alle Pins auf Null stellen
         GPIO.cleanup()
