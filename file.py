eeea0c0eb5bc4d13e9c9a3e2b0df61b42db76b15
jakal1989 <stefano.cugis@gmail.com>
2020-10-25 11:54:01 +0100

    Update file.py

--- file.py
+++ file.py
@@ -1,10 +1,14 @@
+#Importiere Bibliothek
 import os
 
+#Klasse Namens File definieren
 class File:
+    #Statische Methode Namens delete() definieren
     @staticmethod    
     def delete():
         print ("Delete Update list")
+        # Wenn Update_list.txt exisitert diese löschen ansonsten Ausgabe in Konsole Datei existiert nicht
         if os.path.exists("Update_list.txt"):
           os.remove("Update_list.txt")
         else:
-          print("The Update_list file does not exist")
+          print("The Update_list.txt file does not exist")
